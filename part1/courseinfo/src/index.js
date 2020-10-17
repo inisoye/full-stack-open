91e56cc0dc9b88025d351bb639b751807270a387
Inioluwa Akinyosoye <inisoye@gmail.com>
2020-10-17 20:26:29 +0100

    Update courseinfo data

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -2,22 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <>
-      <Header course={course} />
-      <Content
-        parts={[part1, part2, part3]}
-        exercises={[exercises1, exercises2, exercises3]}
-      />
-      <Total exercises={[exercises1, exercises2, exercises3]} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total exercises={course.parts} />
     </>
   );
 };
@@ -28,24 +35,29 @@ const Header = ({ course }) => (
   </>
 );
 
-const Content = ({ parts, exercises }) => {
-  const partComponents = parts.map((eachPart, index) => {
-    return <Part part={eachPart} key={index} exercise={exercises[index]} />;
-  });
-
-  return <>{partComponents}</>;
+const Content = ({ parts }) => {
+  return (
+    <>
+      <Part part={parts[0].name} exercises={parts[0].exercises} />
+      <Part part={parts[1].name} exercises={parts[1].exercises} />
+      <Part part={parts[2].name} exercises={parts[2].exercises} />
+    </>
+  );
 };
 
-const Part = ({ part, exercise }) => (
+const Part = ({ part, exercises }) => (
   <p>
-    {part} {exercise}
+    {part} {exercises}
   </p>
 );
 
 const Total = ({ exercises }) => {
-  const exercisesSum = exercises.reduce((sum, current) => sum + current, 0);
-
-  return <p>Number of exercises {exercisesSum}</p>;
+  return (
+    <p>
+      Number of exercises{' '}
+      {exercises[0].exercises + exercises[1].exercises + exercises[2].exercises}
+    </p>
+  );
 };
 
 ReactDOM.render(
