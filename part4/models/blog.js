658a9408c4682aeaec892e484ea13b16f2fb4463
Inioluwa Akinyosoye <inisoye@gmail.com>
2020-11-04 22:49:40 +0100

    Modify backend with separate models, utils and controllers directories

---
+++ part4/models/blog.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const config = require('../utils/config');
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+const mongoUrl = config.MONGO_URI;
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+module.exports = Blog;
