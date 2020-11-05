32893c85ae7b83379fce625d68b7a17f0a70e13b
Inioluwa Akinyosoye <inisoye@gmail.com>
2020-11-05 04:50:53 +0100

    Refactor main app file into separate modules

--- part4/index.js
+++ part4/index.js
@@ -1,17 +1,11 @@
+const app = require('./app');
 const http = require('http');
-const express = require('express');
-const app = express();
-const cors = require('cors');
 
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 
-const blogsRouter = require('./controllers/blogs');
-app.use('/api/blogs', blogsRouter);
+const server = http.createServer(app);
 
-app.use(cors());
-app.use(express.json());
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
 });
--- part4/models/blog.js
+++ part4/models/blog.js
@@ -9,14 +9,12 @@ const blogSchema = new mongoose.Schema({
   likes: Number,
 });
 
-const Blog = mongoose.model('Blog', blogSchema);
-
-const mongoUrl = config.MONGO_URI;
-mongoose.connect(mongoUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
 });
 
-module.exports = Blog;
+module.exports = mongoose.model('Blog', blogSchema);
