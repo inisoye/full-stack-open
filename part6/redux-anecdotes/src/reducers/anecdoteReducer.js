6b51b1dcfe53d59f7e836e932b0d8734aa9f98f9
Inioluwa Akinyosoye <inisoye@gmail.com>
2021-06-21 11:16:54 +0100

    Exercise 6.14: make anecdote creation asynchronous

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -33,10 +33,10 @@ const vote = (id) => {
   };
 };
 
-const createAnecdote = (content) => {
+const createAnecdote = (data) => {
   return {
     type: 'NEW_ANECDOTE',
-    data: { content, votes: 0 },
+    data,
   };
 };
 
