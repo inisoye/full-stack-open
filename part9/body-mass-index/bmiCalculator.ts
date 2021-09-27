8c1171e90734a3cef1060cf5885f2bae144c02f5
Inioluwa Akinyosoye <inisoye@gmail.com>
2021-09-27 06:10:08 +0100

    Add 9.3

--- part9/body-mass-index/bmiCalculator.ts
+++ part9/body-mass-index/bmiCalculator.ts
@@ -1,6 +1,25 @@
+interface ParsedBmiArgs {
+  height: number;
+  weight: number;
+}
+
+const parseBmiArguments = (args: Array<string>): ParsedBmiArgs => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3]),
+    };
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+};
+
 const calculateBmi = (height: number, weight: number): string => {
   if (height === 0) {
-    return 'Error: height must be greater than zero';
+    throw new Error('Height must be greater than zero!');
   }
 
   const bmi = weight / (height / 100) ** 2;
@@ -12,8 +31,14 @@ const calculateBmi = (height: number, weight: number): string => {
   } else if (bmi >= 25) {
     return 'Overweight';
   } else {
-    return 'Error: ensure you have entered nonzero numbers for height and weight';
+    throw new Error('Ensure you have entered nonzero numbers!');
   }
 };
 
-console.log(calculateBmi(180, 74));
+try {
+  const { height, weight } = parseBmiArguments(process.argv);
+  const bmi = calculateBmi(height, weight);
+  console.log(bmi);
+} catch (e) {
+  console.log('Error, something bad happened, message: ', e);
+}
--- part9/body-mass-index/exerciseCalculator.ts
+++ part9/body-mass-index/exerciseCalculator.ts
@@ -1,3 +1,26 @@
+interface ParsedExerciseArgs {
+  dailyExercises: Array<number>;
+  target: number;
+}
+
+const parseExerciseArguments = (args: Array<string>): ParsedExerciseArgs => {
+  // Allow a minimum of two days
+  if (args.length < 5) throw new Error('Not enough arguments');
+
+  // Pick out all relevant arguments and convert them to numbers
+  const numericArguments = [...args.slice(2)].map((arg) => Number(arg));
+  const areAllArgumentsNumbers = numericArguments.every((arg) => !isNaN(arg));
+
+  if (areAllArgumentsNumbers) {
+    return {
+      dailyExercises: [...numericArguments.slice(1)],
+      target: numericArguments[0],
+    };
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+};
+
 interface exercisesResult {
   periodLength: number;
   trainingDays: number;
@@ -45,4 +68,12 @@ const calculateExercises = (
   };
 };
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+// console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+
+try {
+  const { dailyExercises, target } = parseExerciseArguments(process.argv);
+  const exercisesResult = calculateExercises(dailyExercises, target);
+  console.log(exercisesResult);
+} catch (e) {
+  console.log('Error, something bad happened, message: ', e);
+}
